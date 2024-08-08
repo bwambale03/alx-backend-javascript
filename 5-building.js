6ac9d047f70880fb4934c41cf6d4fe51c0b78199
Edwin Bwambale <precious@kali>
2024-08-09 00:32:44 +0300

    modified:   5-building.js

--- 5-building.js
+++ 5-building.js
@@ -1,25 +1,21 @@
-export default class Building{
-    constructor(sqft){
-        this.sqft = sqft
-        
-        if (this.constructor !== Building){
-            if(typeof this.evacuationWarningMessage !== 'function'){
-                throw new Error(
-                    'Class extending Building must override evacuationWarningMessage'
-                )
-            }
+export default class Building {
+    constructor(sqft) {
+      this.sqft = sqft;
+      if (this.constructor !== Building) {
+        if (typeof this.evacuationWarningMessage !== 'function') {
+          throw new Error(
+            'Class extending Building must override evacuationWarningMessage',
+          );
         }
+      }
     }
-
-    get sqft(){
-        return this._sqft
+  
+    get sqft() {
+      return this._sqft;
     }
-    
-    set sqft(value){
-        if (value != Number){
-            throw new TypeError('Value must be a number! ')
-        }
-        return this._sqft
+  
+    set sqft(value) {
+      this._sqft = value;
     }
-
-}
+  }
+  
