f7abcf1ca865429fd871ca259f3126f80f1f0019
Edwin Bwambale <precious@kali>
2024-08-08 16:33:06 +0300

    updated the file 0-classroom.js

--- 0-classroom.js
+++ 0-classroom.js
@@ -1,5 +1,13 @@
-export default class ClassRoom{
-    constructor(maxStudentsSize){
-        this._maxStudentsSize = maxStudentsSize;
-    }
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
