ebd74ef0006a1a32b3e5a136c81647e64d3a4008
Edwin Bwambale <precious@kali>
2024-08-09 00:33:44 +0300

    modified:   6-sky_high.js

--- 6-sky_high.js
+++ 6-sky_high.js
@@ -1,21 +1,20 @@
-import Building from "./5-building";
+import Building from './5-building';
 
-export default class SkyHighBuilding extends Building{
-    constructor(sqft, floors){
-        super(sqft) = sqft;
-        this.floors = floors;
-    }
-    
-    get floors(){
-        return this._floors;
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
 
-    set floors(value){
-        this.floors = value;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    evacuationWarningMessage(){
-        return `Evacuate slowly the ${this.floors} floors`;
-    }
+  set floors(value) {
+    this._floors = value;
+  }
 
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
