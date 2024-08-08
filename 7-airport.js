d7b8be9c9f47c93270a6db458e358c97515692db
Edwin Bwambale <precious@kali>
2024-08-09 00:35:28 +0300

    modified:   7-airport.js

--- 7-airport.js
+++ 7-airport.js
@@ -1,28 +1,27 @@
 export default class Airport {
     constructor(name, code) {
-        this._name = name;
-        this._code = code;
+      this.name = name;
+      this.code = code;
     }
-
+  
     get name() {
-        return this._name;
+      return this._name;
     }
-
+  
     set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Value must be a string!');
-        }
-        this._name = value;
+      this._name = value;
     }
-
+  
     get code() {
-        return this._code;
+      return this._code;
     }
-
+  
     set code(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Value must be a string!');
-        }
-        this._code = value;
+      this._code = value;
     }
-}
+  
+    get [Symbol.toStringTag]() {
+      return this._code;
+    }
+  }
+  
