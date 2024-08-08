0cf746aad470410db00d50cb7fd88067e297c192
Edwin Bwambale <precious@kali>
2024-08-09 00:36:26 +0300

    modified:   8-hbtn_class.js

--- 8-hbtn_class.js
+++ 8-hbtn_class.js
@@ -1,41 +1,33 @@
-export default class HolbertonClass{
-    constructor(size, location){
-        this.size = size;
-        this.location = location;
+export default class HolbertonClass {
+    constructor(size, location) {
+      this.size = size;
+      this.location = location;
     }
-    // When the class is cast to a number, return the size
-    valueOf(){
-        return this._size
+  
+    get size() {
+      return this._size;
     }
-    // When the class is cast to a string, return the location
-    toString(){
-        return this._location;
+  
+    set size(value) {
+      this._size = value;
     }
-
-    get size(){
-        return this._size;
+  
+    get location() {
+      return this._location;
     }
-
-    set size(value){
-        if( typeof value !== 'number'){
-            throw new TypeError('value should be a number!')
-        }
-        this._size = value;
+  
+    set location(value) {
+      this._location = value;
     }
-
-    get location(){
-        return this._location;
+  
+    [Symbol.toPrimitive](hint) {
+      if (hint === 'number') {
+        return this.size;
+      }
+      if (hint === 'string') {
+        return this.location;
+      }
+      return this;
     }
-
-    set location(value){
-        if (typeof value !== 'string'){
-            throw new  Error('valueOf location should be a String!');
-        }
-        this._location = value;
-    }
-}
-
-/*const myObject = new HolbertonClass(100, 'New York');
-console.log(Number(myObject));
-console.log(String(myObject));
-*/
+  }
+  
