5435b08faa7f969a65cccadcd6c19c4970d3817e
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-10-01 23:01:55 +0300

    modified 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
-ClassRoom = require('./0-classroom');
+import ClassRoom from './0-classroom';
 
 /**
  * Creates an array of {@link ClassRoom}s with a specific size.
  * @returns An array of {@link ClassRoom}s.
  */
-function initializeRooms() {
+export default function initializeRooms() {
   return [19, 20, 34].map((size) => new ClassRoom(size));
 }
-module.exports = initializeRooms;
