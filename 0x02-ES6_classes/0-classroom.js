ae8daa58773269ef4b34b8f1ed408572fc7707cf
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-10-01 22:57:14 +0300

    modified:   0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,13 @@
- class ClassRoom{
-    constructor(maxStudentsSize ){
-        this._maxStudentsSize = maxStudentsSize;
-    }
+/**
+ * Represents a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
-module.exports = ClassRoom;
