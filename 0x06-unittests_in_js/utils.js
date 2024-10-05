3883a5f3e1c80da170d384fed21fed7fd3655218
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-10-05 20:41:28 +0300

    modified:   utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,17 +1,17 @@
-const calculateNumber = require("./0-calcul");
-
 const Utils = {
-    calculateNumber(type, a,b){
-        if (type === 'SUM'){
+    calculateNumber(type, a, b) {
+      if (type === 'SUM') {
         return Math.round(a) + Math.round(b);
-        }
-        if (type === 'SUBTRACT'){
-            return Math.round(a) - Math.round(b);
-        }
-        if (type === 'DIVIDE'){
-            return Math.round(b) === 0 ? 'Error': Math.round(a) / Math.round(b);
-        }
-        return 0;
+      }
+      if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      if (type === 'DIVIDE') {
+        return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+      }
+      return 0;
     },
-};
-moodule.exports = Utils;
+  };
+  
+  module.exports = Utils;
+  
