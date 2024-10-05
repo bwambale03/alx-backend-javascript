7622d8e61b0863d4c1492cf8949fc701ff576312
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-10-05 12:41:23 +0300

    modified:   6-payment_token.js

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,7 @@
+const getPaymentTokenFromAPI = (success) => ((resolve, _reject)=>
+{
+    if(success){
+        resolve({data: 'successful response fro API'});
+    }
+});
+module.exports = getPaymentTokenFromAPI;
